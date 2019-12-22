1bc1c62c87040b4dd1b788f71644432615e8bbbd
datnt-dev <datnt.uet@gmail.com>
2019-12-22 14:47:58 +0700

    fixed

--- database.py
+++ database.py
@@ -10,19 +10,11 @@ password = 'Hoplong6688'
 def get_connection():
     try:
         cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-<<<<<<< HEAD
         print('Connection has been initial...')
-=======
-        # print('Connection has been initial...')
->>>>>>> b90a14cf475b1f428c9da3d7fd6e0579cba7fc96
 
         return cnxn
     except Exception as e:
         print('Connection error: ', e)
         return None
 
-<<<<<<< HEAD
 get_connection()
-=======
-# get_connection()
->>>>>>> b90a14cf475b1f428c9da3d7fd6e0579cba7fc96
